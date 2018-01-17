90094ff1eaa124edf2f411e09afbf940f0202006
grunners <grunners22@hotmail.com>
2018-01-17 16:19:13 +0000

    added some test connection strings

--- server.js
+++ server.js
@@ -17,10 +17,24 @@ app.use(bodyParser.json())
 
 // Connection string parameters.
 var sqlConfig = {
-    user: 'sa',
-    password: 'testy1',
-    server: 'localhost\\sqlexpress',
-    database: 'test'
+    //***home***
+    // user: 'sa',
+    // password: 'testy1',
+    // server: 'localhost\\sqlexpress',
+    // database: 'test'
+
+    //***NCL website***
+    // user: 'ncluser',
+    // password: 'N0v4services',
+    // server: '82.113.145.162\\SQLEXPRESS',
+    // database: 'NCL'
+
+    //***NovaMaster***
+    //driver: 'msnodesqlv8',
+    user: 'Intranet',
+    password: 'Lucy2666213^',
+    server: 'nclsqlsrv001',
+    database: 'NovaMaster'
 }
 
 // Start server and listen on http://localhost:8081/
@@ -33,13 +47,31 @@ var server = app.listen(8080, function () {
 
 // *** queries ***
 
-// GET (select)
+// GET (select) - names
 app.get('/names', function (req, res) {
     sql.connect(sqlConfig, function(err) {
         if (!err) {
             console.log("Request successful");
             var request = new sql.Request();
-            request.query('select name from users', function(err, recordset) {
+            request.query('select name from tblTest', function(err, recordset) {
+                if(err) console.log(err);
+                res.end(JSON.stringify(recordset[0])); // Result in JSON format
+            });
+        } 
+        else {
+            console.log("Error while connecting to database: " + err);
+        }
+        
+    });
+})
+
+// GET (select) - client refs
+app.get('/refs', function (req, res) {
+    sql.connect(sqlConfig, function(err) {
+        if (!err) {
+            console.log("Request successful");
+            var request = new sql.Request();
+            request.query('select ClientRef from tblPersonal', function(err, recordset) {
                 if(err) console.log(err);
                 res.end(JSON.stringify(recordset)); // Result in JSON format
             });
@@ -61,7 +93,7 @@ app.post('/names/new', function (req, res) {
     sql.connect(sqlConfig, function(err) {
         if (!err) {
             var request = new sql.Request();
-            request.query("INSERT INTO users (name, email) VALUES ('" + name + "', '" + email + "')", function(err, recordset) {
+            request.query("INSERT INTO tblTest (name, email) VALUES ('" + name + "', '" + email + "')", function(err, recordset) {
                 if(err) console.log(err);
                 res.end(JSON.stringify(recordset)); // Result in JSON format
             });
